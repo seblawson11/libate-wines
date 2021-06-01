d17772ddba6169fafc6f80ea29d540fb746666fc
Seb Lawson <seblawson11@gmail.com>
2021-06-01 21:58:01 +0100

    removed page

--- src/components/sections/header.js
+++ src/components/sections/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
-import MailChimpForm from './MailChimpForm'
+import MailChimpForm from './mailchimpForm'
 
 import { Container } from "../global"
 
